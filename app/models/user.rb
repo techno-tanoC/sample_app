43992336ce6ff59b80116baf3260a40a6f63511d
techno-tanoC <tech@MacBook-Pro.local>
2014-05-09 17:12:41 +0900

    8.22

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ActiveRecord::Base
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 } 
+
+  def User.new_rememeber_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_rememeber_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
   resources :users
-
-#  get 'users/new'
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'static_pages#home'
 
   match '/signup', to: 'users#new', via: 'get'
+  
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
 #  get 'static_pages/home'
   match '/home', to: 'static_pages#home', via: 'get'
-
 #  get 'static_pages/help'
   match '/help', to: 'static_pages#help', via: 'get'
-
 #  get 'static_pages/about'
   match '/about', to: 'static_pages#about', via: 'get'
-
 #  get 'static_pages/contact'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
